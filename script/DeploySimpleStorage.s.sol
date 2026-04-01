d08ea550d2b70a73e0f248c604e7601f870c70b0
Rasheed <hidrahtweet@gmail.com>
2026-04-01 23:22:36 +0100

    scripted

--- script/DeploySimpleStorage.s.sol
+++ script/DeploySimpleStorage.s.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
-import "forge-std/Script.sol";
+pragma solidity ^0.8.19;
+import {Script} from "forge-std/Script.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
-    function run() external {
+    function run() external returns (SimpleStorage) {
         vm.startBroadcast();
-        new SimpleStorage();
+        SimpleStorage simpleStorage = new SimpleStorage();
         vm.stopBroadcast();
+        return simpleStorage;
     }
 }
