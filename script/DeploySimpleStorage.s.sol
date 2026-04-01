ca167619a124970b82a5491149dbef5dccdf49f3
Rasheed <hidrahtweet@gmail.com>
2026-04-01 22:45:51 +0100

    Coopilot could just be scripting for me

---
+++ script/DeploySimpleStorage.s.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+import "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external {
+        vm.startBroadcast();
+        new SimpleStorage();
+        vm.stopBroadcast();
+    }
+}
